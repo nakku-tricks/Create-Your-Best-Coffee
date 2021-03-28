b9cf8c61691e7874719d498ddf5b83c2504e4680
Maxim <nakkutricks@gmail.com>
2021-03-28 21:16:58 +0700

    Изменил название файла на корректное

---
+++ src/views/ViewHome.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="view-home">
+    <the-header></the-header>
+    <the-main></the-main>
+  </div>
+</template>
+
+<script>
+import TheHeader from "../components/TheHeader";
+import TheMain from "../components/TheMain";
+export default {
+  components: {
+    TheHeader,
+    TheMain,
+  },
+};
+</script>
+
+<style lang="scss">
+.view-home {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+</style>
