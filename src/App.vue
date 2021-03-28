0c81c3c97ecedcfd29df9f60d6b547b02d66e0b4
Maxim <nakkutricks@gmail.com>
2021-03-28 15:32:23 +0700

    Импортировал главные компоненты в App

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
-  <div id="app">Hello world</div>
+  <div id="app">
+    <the-header></the-header>
+    <the-main></the-main>
+  </div>
 </template>
 
 <script>
+import TheHeader from "./components/TheHeader";
+import TheMain from "./components/TheMain";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    TheHeader,
+    TheMain,
+  },
 };
 </script>
 
 <style lang="scss">
 #app {
+  width: 100%;
+  height: 100%;
+
+  padding: 48px 64px;
+
+  background: #fcfcfc;
 }
 </style>
