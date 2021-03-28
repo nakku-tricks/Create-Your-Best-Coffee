745311435e70c1638b3a211d48aea56efabe2bac
Maxim <nakkutricks@gmail.com>
2021-03-28 21:18:42 +0700

    Сверстал компонент списка

---
+++ src/components/CoffeeList.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="coffee-list">
+    <ul class="coffee-list__list">
+      <coffee-list-item></coffee-list-item>
+    </ul>
+  </section>
+</template>
+
+<script>
+import CoffeeListItem from "./CoffeeListItem";
+export default {
+  components: {
+    CoffeeListItem,
+  },
+};
+</script>
+
+<style lang="scss">
+.coffee-list {
+  height: 90%;
+
+  &__list {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+    margin: 0;
+    padding: 0;
+  }
+}
+</style>
