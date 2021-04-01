f4376d404a36c0defc0c0bcfe5240887b6d3650b
Maxim <nakkutricks@gmail.com>
2021-04-01 22:24:59 +0700

    Реализовал получение данных из api

---
+++ src/storage/modules/Coffees.js
@@ -0,0 +1,20 @@
+import { API_URL } from "@/constants";
+export default {
+  state: {
+    coffee: [],
+  },
+  actions: {
+    async getCoffeeList({ commit }) {
+      fetch(API_URL)
+        .then((value) => value.json())
+        .then((value) => commit("setCoffeeToState", value))
+        .catch((error) => console.log(error));
+    },
+  },
+  mutations: {
+    setCoffeeToState(state, value) {
+      state.coffee = value;
+    },
+  },
+  getters: {},
+};
