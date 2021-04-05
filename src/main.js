ce60fd59011d49e7ab238606eb3bd651c137bb0d
Maxim <nakkutricks@gmail.com>
2021-04-05 11:48:21 +0700

    Подключил vue-router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./storage/store";
+import VueRouter from "vue-router";
+import router from "./router/router";
+
+Vue.use(VueRouter);
 
 import "./assets/styles/index.scss";
 
@@ -11,4 +15,5 @@ new Vue({
     return h(App);
   },
   store,
+  router,
 }).$mount("#app");
