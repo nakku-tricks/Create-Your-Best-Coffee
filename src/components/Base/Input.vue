c09dbf0fd09db10cf302687f1636deac394103c5
Maxim <nakkutricks@gmail.com>
2021-04-10 11:35:59 +0700

    Базовый инпут

---
+++ src/components/Base/Input.vue
@@ -0,0 +1,27 @@
+<template>
+  <input class="base-input" type="text" v-model.trim="getValue" />
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: [String, Number],
+      default: "",
+    },
+  },
+  computed: {
+    getValue: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("setValue", value);
+      },
+    },
+  },
+};
+</script>
+
+<style>
+</style>
